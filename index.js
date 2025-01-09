3ec35a68930967f4ca83afa18afeb48288335b31
mrhl082024 <mrhl082024@gmail.com>
2025-01-09 08:54:58 +0100

    løst første del

---
+++ index.js
@@ -0,0 +1,12 @@
+const products = [
+    { name: "Smartphone", price: 800, category: "electronics" },
+    { name: "Headphones", price: 150, category: "electronics" },
+    { name: "Coffee Maker", price: 100, category: "home appliances" },
+    { name: "Blender", price: 250, category: "home appliances" },
+    { name: "Laptop", price: 1200, category: "electronics" },
+    { name: "T-shirt", price: 50, category: "clothing" },
+    { name: "Sneakers", price: 300, category: "clothing" }
+];
+
+const billig = products.filter((product) => product.price < 200);
+console.table(billig)
